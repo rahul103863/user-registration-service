ba233cbd8c1885c8e5f60cb2a23c65d0f53f2958
RahulKuchuru <rahul.kuchuru@westjet.com>
2022-07-26 00:26:42 -0400

    updated package name

--- src/test/java/com/myorg/registration/RegistrationServiceApplicationTests.java
+++ src/test/java/com/myorg/registration/RegistrationServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.xerris.registration;
+package com.myorg.registration;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
